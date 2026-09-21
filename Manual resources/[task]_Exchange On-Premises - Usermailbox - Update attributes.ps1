d98182b6f77d14f2a3052c698b6093a171c315ea
Jerry Breek <69583791+jbreek@users.noreply.github.com>
2026-09-21 15:32:33 +0200

    Release v2 (#1)
    * Release v2
    
    * Added workflows
    
    Added workflows
    Fixed variable
    
    ---------
    
    Co-authored-by: rscholtelubberink <80954753+rscholtelubberink@users.noreply.github.com>

---
+++ Manual resources/[task]_Exchange On-Premises - Usermailbox - Update attributes.ps1
@@ -0,0 +1,154 @@
+# Variables configured in form
+$mailbox = $form.selectedmailbox
+$MailboxDisplayName = $form.textDisplayName
+
+# Global variables
+# Outcommented as these are set from Global Variables
+# $ExchangeConnectionUri = ""
+# $ExchangeAdminUsername = ""
+# $ExchangeAdminPassword = ""
+
+# Fixed values
+$commands = @(
+    "Set-Mailbox"
+)
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+$VerbosePreference = "SilentlyContinue"
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+
+#region functions
+#endregion functions
+
+# Connect to Exchange
+try {
+    # Create credentials
+    $actionMessage = "creating credentials object"
+    
+    $securePassword = ConvertTo-SecureString -String $ExchangeAdminPassword -AsPlainText -Force
+    $credential = [System.Management.Automation.PSCredential]::new($ExchangeAdminUsername, $securePassword)
+    
+    Write-Verbose "Created credentials for user [$ExchangeAdminUsername]"
+
+    # Connect to Exchange On-Premises
+    # Docs: https://learn.microsoft.com/en-us/powershell/exchange/connect-to-exchange-servers-using-remote-powershell
+    $actionMessage = "connecting to Exchange On-Premises"
+
+    $sessionOptionParams = @{
+        SkipCACheck         = $false
+        SkipCNCheck         = $false
+        SkipRevocationCheck = $false
+    }
+
+    $sessionOption = New-PSSessionOption @sessionOptionParams
+
+    $sessionParams = @{
+        Authentication    = 'Default'
+        ConfigurationName = 'Microsoft.Exchange'
+        Credential        = $credential
+        ConnectionUri     = $ExchangeConnectionUri
+        SessionOption     = $sessionOption
+        ErrorAction       = "Stop"
+    }
+
+    $exchangeSession = New-PSSession @sessionParams
+    $null = Import-PSSession -Session $exchangeSession -DisableNameChecking -AllowClobber -CommandName $commands -ErrorAction Stop
+
+    # Send initial audit log
+    $Log = @{
+        Action            = "UpdateAccount" # optional. ENUM (undefined = default) 
+        System            = "Exchange On-Premises" # optional (free format text) 
+        Message           = "Successfully connected to Exchange using URI [$ExchangeConnectionUri]" # required (free format text) 
+        IsError           = $false # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+        TargetDisplayName = $ExchangeConnectionUri # optional (free format text) 
+        TargetIdentifier  = $([string]$exchangeSession.InstanceId) # optional (free format text) 
+    }
+    Write-Information -Tags "Audit" -MessageData $log
+
+    $exchangeSetMailboxParams = @{
+        identity = $mailbox.identity
+        ErrorAction = 'Stop'
+    } 
+
+    if ( -not ($null -eq $MailboxDisplayName)) {      
+        $exchangeSetMailboxParams.add("DisplayName", $MailboxDisplayName)     
+    }
+    
+    for ($i = 1; $i -le 15; $i++) {
+        if($null -ne $form."textCA$i"){
+            $exchangeSetMailboxParams.add("CustomAttribute$i",$form."textCA$i")
+        }    
+    }
+
+    $actionMessage = "setting mailbox attributes"  
+    $null = Set-Mailbox @exchangeSetMailboxParams
+     
+    Write-Information "Succesfully updated mailbox attributes for [$($mailbox.DisplayName) ($($mailbox.ExchangeGuid))]"
+    $Log = @{
+        Action            = "UpdateAccount" # optional. ENUM (undefined = default) 
+        System            = "Exchange On-Premises" # optional (free format text) 
+        Message           = "Succesfully updated mailbox attributes for [$($mailbox.DisplayName) ($($mailbox.ExchangeGuid))]." # required (free format text) 
+        IsError           = $false # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+        TargetDisplayName = $mailbox.DisplayName # optional (free format text) 
+        TargetIdentifier  = $mailbox.ExchangeGuid # optional (free format text) 
+    }
+    #send result back  
+    Write-Information -Tags "Audit" -MessageData $log
+}
+catch {
+    $ex = $PSItem
+    if (-not [string]::IsNullOrEmpty($ex.Exception.Message)) {
+        $warningMessage = "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+        $auditMessage = "Error $($actionMessage). Error: $($ex.Exception.Message)"
+    }
+    else {
+        $warningMessage = "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($ex.Exception)"
+        $auditMessage = "Error $($actionMessage). Error: $($ex.Exception)"
+    }
+
+    # Send error audit log to HelloID
+    $Log = @{
+        Action            = "UpdateAccount" # optional. ENUM (undefined = default) 
+        System            = "Exchange On-Premises" # optional (free format text) 
+        Message           = $auditMessage # required (free format text) 
+        IsError           = $true # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+        TargetDisplayName = $mailbox.DisplayName # optional (free format text) 
+        TargetIdentifier  = $mailbox.ExchangeGuid # optional (free format text) 
+    }
+    
+    Write-Information -Tags "Audit" -MessageData $log
+    Write-Warning $warningMessage
+    Write-Error $auditMessage
+}
+finally {
+    # Disconnect from Exchange
+    # Docs: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/remove-pssession
+    if ($null -ne $exchangeSession) {
+        try {
+            $deleteExchangeSessionSplatParams = @{
+                Session     = $exchangeSession
+                Confirm     = $false
+                ErrorAction = "Stop"
+            }
+            $null = Remove-PSSession @deleteExchangeSessionSplatParams
+
+            # Send disconnect audit log
+            $Log = @{
+                Action            = "UpdateAccount" # optional. ENUM (undefined = default) 
+                System            = "Exchange On-Premises" # optional (free format text) 
+                Message           = "Successfully disconnected from Exchange using URI [$ExchangeConnectionUri]" # required (free format text) 
+                IsError           = $false # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+                TargetDisplayName = $ExchangeConnectionUri # optional (free format text) 
+                TargetIdentifier  = $([string]$exchangeSession.InstanceId) # optional (free format text) 
+            }
+            Write-Information -Tags "Audit" -MessageData $log
+        }
+        catch {
+            Write-Warning "Failed to disconnect from Exchange using URI [$ExchangeConnectionUri]. Error: $($_.Exception.Message)"
+        }
+    }
+}
